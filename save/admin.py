7c23b02a008c2c3546deb191d28fba5534557da9
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-29 08:15:02 +0300

    add admin.py

--- save/admin.py
+++ save/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models  import *
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Finance)
+admin.site.register(SavingPlan)
+admin.site.register(Month)
